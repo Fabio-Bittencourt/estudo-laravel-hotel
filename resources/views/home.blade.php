5a6f289cb8191e9f7f20ba5edd32f25ac54955d3
Fabio-Bittencourt <fabio_bittencourt999@yahoo.com.br>
2018-04-25 17:37:44 -0300

    First commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('index')
+@section('content')
+<div class='col-md-auto'>
+    <h1>Seja Bem vindo ao Hotel </h1>
+</div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Hotel Resort Bortot</title>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+	<!-- Fonts -->
+	<link href='//fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+        <!-- Styles -->
+        <link href="/css/main.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <nav class='navbar navbar-light bg-light'>
+            <ul class='nav-bar nav'>
+                <li class='nav-item'><a  class='nav-link' href='/'>Home</a></li>
+                <li class='nav-item'><a  class='nav-link' href='#'>Hospede</a></li>
+                <li class='nav-item'><a  class='nav-link' href='#'>Reservas</a></li>
+                <li class='nav-item'><a  class='nav-link' href='#'>Checkin</a></li>
+                <li class='nav-item'><a  class='nav-link' href='#'>Checkout</a></li>
+            </ul>
+        </nav>
+<div class="container">
+    <div class='row'>
+            @yield('content')
+    </div>
+</div>
+<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    </body>
+</html>
